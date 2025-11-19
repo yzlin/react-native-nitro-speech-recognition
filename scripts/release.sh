d42c82c2c0e9f44a2a226520c4cf9e2bb99059b9
Ethan Lin <yzlin1985@gmail.com>
2025-11-19 14:39:13 +0800

    build: bundle readme in npm package

--- scripts/release.sh
+++ scripts/release.sh
@@ -6,8 +6,10 @@ echo "Starting the release process..."
 echo "Provided options: $@"
 
 echo "Publishing 'react-native-nitro-speech-recognition' to NPM"
+cp README.md packages/react-native-nitro-speech-recognition/
 cd packages/react-native-nitro-speech-recognition
 bun release $@
+rm README.md
 
 echo "Creating a Git bump commit and GitHub release"
 cd ../..
